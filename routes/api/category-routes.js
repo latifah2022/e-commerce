b42468bfd65642e6e20a556c301640e504e23272
Latifah Zawedde <zawedde2021@gmail.com>
2022-09-29 19:04:39 -0700

    delete category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,7 +45,7 @@ router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try{
     const category = await Category.update(req.body, { where: {id: req.params.id}})    
-    if (!categoryData){
+    if (!category){
       res.status(404).json({ message: "Location not found" });
     }
     res.status(200).json(category);
@@ -54,8 +54,17 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const category = await Category.destroy( {where: {id: req.params.id}} );
+    if (!category){
+      res.status(404).json({ message: "Location not found" });
+    }
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
