d86fe25a63cef661308153b88d1c24e0532a7a9d
Latifah Zawedde <zawedde2021@gmail.com>
2022-09-29 18:42:59 -0700

    deleting one product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -66,7 +66,6 @@ router.post('/', async (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
 
 // update product
 router.put('/:id', (req, res) => {
@@ -110,8 +109,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const product = await Product.destroy( {where: {id: req.params.id}});
+    if (!product){
+      res.status(404).json({message: "Location not found"});
+    } else {
+      return res.status(200).json(product) 
+    }
+  } catch (err) {
+    res.status(500).json(err)
+}
 });
 
 module.exports = router;
