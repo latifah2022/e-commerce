2f9ea524d4ae38ffa1cb18bd34109e6e30419952
Latifah Zawedde <zawedde2021@gmail.com>
2022-09-29 18:00:44 -0700

    upudate tag route

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  try {
+    const tagg = await Tag.findAll();
+    res.status(200).json(tagg);
+  } catch (err) {
+    res.status(404).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+  try {
+    const tagg = await Tag.findByPk(req.params.id,
+      {
+        include: Product, through: ProductTag
+      });
+    if(!tagg){
+      res.status(404).json({
+        message: "No location found"
+      })
+      return;
+    } 
+    res.status(200).json(tagg)
+  } catch (err) {
+    res.status(404).json(err)
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new tag
+  try {
+    const tagg = await Tag.create(req.body)
+    res.status(200).json(tagg)
+  } catch (err) {
+    res.status(404).json(err)
+  }
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  try {
+    const tagg = await Tag.destroy( {where: {id: req.params.id}});
+    if (!tagg){
+      res.status()
+    }
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+});
+
+module.exports = router;
